02ac99abd52f3918b688f62c21fd4446fdc7ac86
Jronin201 <Jronin22@gmail.com>
2025-06-15 19:53:39 -0700

    Update the-one-ring fetch to use deployed backend

--- static/the-one-ring/script.js
+++ static/the-one-ring/script.js
@@ -15,7 +15,7 @@ input.addEventListener("keydown", async (e) => {
     input.value = "";
 
     try {
-      const response = await fetch("/chat", {
+      const response = await fetch("https://mydemerzel-backend.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMessage })
